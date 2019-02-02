53ebbf9ba81b2733ebe85eff4c93fb5583e7b9fe
JoeBew42 <joebew42@gmail.com>
2019-02-02 19:04:11 +0100

    start the command handler as the application start

--- lib/ex_twitch_pomobot/application.ex
+++ lib/ex_twitch_pomobot/application.ex
@@ -1,19 +1,13 @@
 defmodule ExTwitchPomobot.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
+  alias ExTwitchPomobot.CommandHandler
+
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: ExTwitchPomobot.Worker.start_link(arg)
-      # {ExTwitchPomobot.Worker, arg},
+      CommandHandler
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ExTwitchPomobot.Supervisor]
     Supervisor.start_link(children, opts)
   end
